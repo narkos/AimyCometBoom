17ff66a37ced6657d40e5bac3062c797d409f7ed
narkos <narkos.newb@gmail.com>
2018-11-23 11:33:07 +0100

    Initial Commit

---
+++ Assets/SCRIPTS/FollowMouseController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouseController : MonoBehaviour {
+
+	private Vector3 _nextPosition;
+	
+	[SerializeField] private float _moveSpeed = 0.1f;
+
+	void Start() {
+		_nextPosition = transform.position;
+	}
+
+	void Update () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		_nextPosition = new Vector3(Input.mousePosition.x, distance, Input.mousePosition.y);
+		_nextPosition = Camera.main.ScreenToWorldPoint(_nextPosition);
+
+		transform.position = Vector3.Lerp(transform.position, _nextPosition, _moveSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/SCRIPTS/OrbitController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitController : MonoBehaviour
+{
+    public Transform target;
+	public Vector3 targetOffset = Vector3.zero;
+	public float offset = 5.0f;
+	public float rotationSpeed = 1.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 relativePos = (target.position + targetOffset) - transform.position;
+
+		Quaternion rotation = Quaternion.LookRotation(relativePos);
+		Quaternion current = transform.localRotation;
+		float relOffset = rotationSpeed * offset;
+		transform.localRotation = Quaternion.Slerp(current, rotation, rotationSpeed* Time.deltaTime);
+		transform.Translate(0, 0, relOffset* Time.deltaTime);
+    }
+}
